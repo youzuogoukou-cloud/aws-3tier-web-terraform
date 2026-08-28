df07317d376701c9fbbe11a41e969d7353026605
youzuogoukou-cloud <youzuogoukou@gmail.com>
2026-08-28 11:43:30 +0900

    Add HTTPS to the ALB
    443リスナーを追加し、80リスナーを443へリダイレクトに変更
    
    独自ドメインを使用するとコストがかかるため、独自で証明書の発行のためにtlsにて証明書を作成
    
    構築側のPCの時計がずれていたのでtimeを入れ、ACMにインポートする際に有効期限内になるようにtime_sleepで調整

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.14"
+    }
   }
   backend "s3" {
     use_lockfile = true
